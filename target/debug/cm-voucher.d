b080730bb23aede271d7a6f82fdb414a2db5cdc4
Vanderson Antonietti <v.antonietti@icloud.com>
2023-06-22 13:02:43 -0300

    First Commit

---
+++ target/debug/cm-voucher.d
@@ -0,0 +1,1 @@
+/Users/an7onie77i/Code/cm-voucher/target/debug/cm-voucher: /Users/an7onie77i/Code/cm-voucher/src/main.rs
---
+++ target/debug/deps/cm_voucher-1a0941e7ac560666.d
@@ -0,0 +1,5 @@
+/Users/an7onie77i/Code/cm-voucher/target/debug/deps/cm_voucher-1a0941e7ac560666.rmeta: src/main.rs
+
+/Users/an7onie77i/Code/cm-voucher/target/debug/deps/cm_voucher-1a0941e7ac560666.d: src/main.rs
+
+src/main.rs:
